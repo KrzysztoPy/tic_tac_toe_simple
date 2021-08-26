7eff4da881c46f29b9546d5b13e8ad74ede91ecc
Kriss <geraltrivia19@gmail.com>
2021-08-26 12:24:03 +0200

    Created menu_options_text.py file with  displayed text for user.

---
+++ main_menu/menu_options/static_text/menu_options_text.py
@@ -0,0 +1,15 @@
+def game_name_txt():
+    return '\n Simple Tic Tac Toe \n'
+
+
+def menu_option_txt():
+    menu_list = ['1. Stat menu', '2. Statistic', '3. Exit']
+    return menu_list
+
+
+def question_about_selected_option_txt():
+    return '\n Select option: '
+
+
+def wrong_range_txt():
+    return ' You selected options out of range. Please try again.'
