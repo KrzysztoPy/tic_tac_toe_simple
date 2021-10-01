b35ae6177a11719b371cd98b1feb280e70f24b94
Kriss <geraltrivia19@gmail.com>
2021-10-01 14:45:40 +0200

    fixup! Small changes.

--- main_menu/menu.py
+++ main_menu/menu.py
@@ -13,11 +13,11 @@ def selecting_an_action_option():
 
 
 def data_analysis_for_compilance_with_the_guidelines(user_data):
-    input_data = check_which_user_data_is_integer(user_data)
+    user_input_data = check_which_user_data_is_integer(user_data)
 
-    if input_data or input_data == 0:
-        if checking_the_correctness_of_the_range(input_data):
-            return input_data
+    if user_input_data or user_input_data == 0:
+        if checking_the_correctness_of_the_range(user_input_data):
+            return user_input_data
 
 
 def check_which_user_data_is_integer(user_data):
@@ -51,5 +51,3 @@ def go_to_the_selected_option(user_selection):
     elif user_selection == 3:
         exit_game()
 
-
-selecting_an_action_option()
